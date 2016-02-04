efc872602a547fa40b382841a4b1dfdf37406dee
Michal Sanger <michal.sanger@gmail.com>
2016-02-04 16:19:20 +0100

    Test Echo via POST

--- tests/functional/EchoTest.php
+++ tests/functional/EchoTest.php
@@ -1,20 +1,47 @@
 <?php
 
+use Omnipay\Csob\Message\EchoResponse;
+
 class EchoTest extends PHPUnit_Framework_TestCase
 {
-    public function testGet()
+    /** @var \Omnipay\Csob\Gateway */
+    private $gateway;
+
+    private $merchantId = 'A1029DTmM7';
+
+    public function setUp()
     {
         $publicKey = file_get_contents(__DIR__ . '/../unit/Sign/assets/mips_iplatebnibrana.csob.cz.pub');
         $privateKey = file_get_contents(__DIR__ . '/../unit/Sign/assets/rsa_A1029DTmM7.key');
-        $gateway = \Omnipay\Csob\GatewayFactory::createInstance($publicKey, $privateKey);
-        $merchantId = 'A1029DTmM7';
+        $this->gateway = \Omnipay\Csob\GatewayFactory::createInstance($publicKey, $privateKey);
+    }
 
-        $response = $gateway->testGateway($merchantId, 'GET');
+    /**
+     * @see https://github.com/csob/paymentgateway/wiki/How-to-Switch-to-the-Live-Environment#communication-test---echo-get
+     */
+    public function testGet()
+    {
+        $response = $this->gateway->testGateway($this->merchantId, 'GET');
+
+        $this->makeAsserts($response);
+    }
+
+    /**
+     * @see https://github.com/csob/paymentgateway/wiki/How-to-Switch-to-the-Live-Environment#communication-test---echo-post
+     */
+    public function testPost()
+    {
+        $response = $this->gateway->testGateway($this->merchantId, 'POST');
+
+        $this->makeAsserts($response);
+    }
+
+    private function makeAsserts(EchoResponse $response)
+    {
         $data = $response->getData();
-
-        $this->assertArrayHasKey('resultCode', $data);
-        $this->assertArrayHasKey('resultMessage', $data);
-        $this->assertSame(0, $data['resultCode']);
-        $this->assertSame('OK', $data['resultMessage']);
+        $this->assertArrayHasKey('resultCode', $data, 'resultCode is in response data');
+        $this->assertArrayHasKey('resultMessage', $data, 'resultMessage is in response data');
+        $this->assertSame(0, $data['resultCode'], 'Result code is 0');
+        $this->assertSame('OK', $data['resultMessage'], 'Result message is "OK"');
     }
 }
